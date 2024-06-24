d2a612d585d66c86b820f48201db39a32d8c9d56
hafiz-asadpbg <asadullahofficial009@gmail.com>
2024-06-24 13:51:25 +0500

    Add: overView design

---
+++ src/components/stepper/index.tsx
@@ -0,0 +1,162 @@
+import * as React from 'react'
+
+import { styled } from '@mui/material/styles'
+import Stepper from '@mui/material/Stepper'
+import Step from '@mui/material/Step'
+import StepLabel from '@mui/material/StepLabel'
+
+import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector'
+
+import type { StepIconProps } from '@mui/material/StepIcon'
+import { Box, Button, Card, CardActions, CardContent, CardHeader, Typography } from '@mui/material'
+
+const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
+  [`&.${stepConnectorClasses.alternativeLabel}`]: {
+    top: 22
+  },
+  [`&.${stepConnectorClasses.active}`]: {
+    [`& .${stepConnectorClasses.line}`]: {
+      backgroundImage: 'linear-gradient( 95deg,rgb(46,125,50) 0%,rgb(76,171,81) 50%,rgb(217,231,218) 100%)'
+    }
+  },
+  [`&.${stepConnectorClasses.completed}`]: {
+    [`& .${stepConnectorClasses.line}`]: {
+      backgroundImage: 'linear-gradient( 95deg,rgb(46,125,50) 0%,rgb(76,171,81) 50%,rgb(217,231,218) 100%)'
+    }
+  },
+  [`& .${stepConnectorClasses.line}`]: {
+    height: 3,
+    border: 0,
+    backgroundColor: theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
+    borderRadius: 1
+  }
+}))
+
+const ColorlibStepIconRoot = styled('div')<{
+  ownerState: { completed?: boolean; active?: boolean }
+}>(({ theme, ownerState }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#ccc',
+  zIndex: 1,
+  color: '#fff',
+  width: 50,
+  height: 50,
+  display: 'flex',
+  borderRadius: '50%',
+  justifyContent: 'center',
+  alignItems: 'center',
+  ...(ownerState.active && {
+    backgroundImage: 'linear-gradient( 136deg, rgb(46,125,50) 0%, rgb(76,171,81) 50%, rgb(217,231,218) 100%)',
+    boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)'
+  }),
+  ...(ownerState.completed && {
+    backgroundImage: 'linear-gradient( 136deg, rgb(46,125,50) 0%, rgb(76,171,81) 50%, rgb(217,231,218) 100%)'
+  })
+}))
+
+function ColorlibStepIcon(props: StepIconProps) {
+  const { active, completed, className } = props
+
+  const icons: { [index: string]: React.ReactElement } = {
+    1: <i className='ri-account-circle-line'></i>,
+    2: <i className='ri-account-circle-line'></i>,
+    3: <i className='ri-account-circle-line'></i>,
+    4: <i className='ri-account-circle-line'></i>,
+    5: <i className='ri-account-circle-line'></i>,
+    6: <i className='ri-account-circle-line'></i>
+  }
+
+  return (
+    <ColorlibStepIconRoot ownerState={{ completed, active }} className={className}>
+      {icons[String(props.icon)]}
+    </ColorlibStepIconRoot>
+  )
+}
+
+const steps = [
+  { label: 'Lead', date: '2/11/2023', days: '7 Days' },
+  { label: 'Prospect', date: '2/11/2023', days: '7 Days' },
+  { label: 'Approved', date: '2/11/2023', days: '7 Days' },
+  { label: 'Complete', date: '', days: '7 Days' },
+  { label: 'Invoice', date: '', days: '7 Days' },
+  { label: 'Closed', date: '' }
+]
+
+export default function CustomizedSteppers() {
+  return (
+    <Card>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <CardHeader title='Milestone' />
+        <CardHeader subheader='In Prospect Milestone: 7 Months ' />
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'start',
+          alignItems: 'center',
+          gap: 2
+        }}
+      >
+        <CardActions
+          sx={{
+            flexShrink: 0,
+            maxWidth: '100%',
+            display: 'flex',
+            justifyContent: 'start',
+            alignItems: 'start',
+            gap: 2,
+            flexDirection:'column'
+          }}
+        >
+          <Typography
+            variant='h6'
+            sx={{
+              whiteSpace: 'nowrap'
+            }}
+          >
+            <span className='text-primary text-bold'>NEXT STEP :</span> Closed
+          </Typography>
+          <Button variant='contained' sx={{marginLeft: '0px !important'}}>Submit</Button>
+        </CardActions>
+        <CardContent
+          sx={{
+            width: '100%'
+          }}
+        >
+          <Stepper alternativeLabel activeStep={4} connector={<ColorlibConnector />}>
+            {steps.map((step, index) => (
+              <Step key={index}>
+                <StepLabel StepIconComponent={ColorlibStepIcon}>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      gap: 1,
+                      position: 'relative'
+                    }}
+                  >
+                    <Typography variant='h6'>{step.label ? step.label : '--'}</Typography>
+                    <Typography variant='body2'>{step.date ? step.date : '--'}</Typography>
+                  </Box>
+                  {step.days && (
+                    <Box sx={{ position: 'absolute', top: 0, right: 0 }}>
+                      <Typography variant='body2'>{step.days}</Typography>
+                    </Box>
+                  )}
+                </StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </CardContent>
+      </Box>
+    </Card>
+  )
+}
--- src/views/app/appraisers/addAndeditViews/overview/index.tsx
+++ src/views/app/appraisers/addAndeditViews/overview/index.tsx
@@ -1,13 +1,19 @@
 'use client'
 
-import Inprogress from '@/components/inprogressPage/Inprogress'
+import CustomizedSteppers from '@/components/stepper'
+import ContactCard from './cards'
 
 const Overview = () => {
   return (
-    <>
-      <h1>Overview</h1>
-      <Inprogress />
-    </>
+    <div className='grid grid-cols-4 w-full gap-2'>
+      <div className='col-span-3'>
+        <CustomizedSteppers />
+      </div>
+
+      <div className='col-span-1'>
+        <ContactCard />
+      </div>
+    </div>
   )
 }
 
