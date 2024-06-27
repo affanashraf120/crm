a666b6a515af106e17e6a637c0bfc12616c2c5cf
hafiz-asadpbg <asadullahofficial009@gmail.com>
2024-06-27 11:34:13 +0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,18 @@
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export function middleware(request: Request) {
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl;
+
+  // Check if the request is for the root path
+  if (url.pathname === '/') {
+    // Redirect to the dashboard page
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
 
   // Store current request url in a custom header, which you can read later
   const requestHeaders = new Headers(request.headers);
-  
+
   requestHeaders.set('pathname', request.url);
 
   return NextResponse.next({
@@ -12,4 +20,8 @@ export function middleware(request: Request) {
       headers: requestHeaders,
     }
   });
+}
+
+export const config = {
+  matcher: ['/', '/your-other-paths'] // Add other paths if needed
 }
